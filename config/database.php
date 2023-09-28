b378db968782374c56e161a1fc416a928250914a
Islam Mac <islam@mac.com>
2023-09-28 23:28:23 +0300

    adding another database connection

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
       ]) : [],
     ],
 
+    'creditTransfer' => [
+      'driver' => 'mysql',
+      'url' => env('DATABASE_URL'),
+      'host' => env('DB_HOST2', '127.0.0.1'),
+      'port' => env('DB_PORT2', '3306'),
+      'database' => env('DB_DATABASE2', 'forge'),
+      'username' => env('DB_USERNAME2', 'forge'),
+      'password' => env('DB_PASSWORD2', ''),
+      'unix_socket' => env('DB_SOCKET', ''),
+      'charset' => env('DB_CHARSET', 'utf8'),
+      'collation' => env('DB_COLLATE2', 'UTF8_general_ci'),
+      'prefix' => '',
+      'prefix_indexes' => true,
+      'strict' => true,
+      'engine' => null,
+      'options' => extension_loaded('pdo_mysql') ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+      ]) : [],
+    ],
+
     'pgsql' => [
       'driver' => 'pgsql',
       'url' => env('DATABASE_URL'),
